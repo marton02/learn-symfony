e58ef7b01264a89967ea8697340fb72c992d18aa
Márton Molnár <marton3molnar@gmail.com>
2024-06-20 20:42:33 +0200

    initial commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HelloController extends AbstractController
+{
+
+    private array $messages = [
+        "Hello", "Hi", "Bye"
+    ];
+
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' =>$limit,
+            ]
+        );
+    }
+
+    #[Route("/messages/{id<\d+>}",name:"app_show_one")]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+    }
+}
