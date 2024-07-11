2e79d728b069089f080981cb4bf6cc89a36a39df
Márton Molnár <marton3molnar@gmail.com>
2024-07-11 15:53:03 +0200

    Asign users to microposts and comments

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -44,18 +44,21 @@ class AppFixtures extends Fixture
         $microPost1 = new MicroPost();
         $microPost1->setTitle("Welcome to Hungary!");
         $microPost1->setText("Welcome to Hungary");
+        $microPost1->setAuthor($user1);
         $microPost1->setCreatedAt(new DateTime());
         $manager->persist($microPost1);
 
         $microPost2 = new MicroPost();
         $microPost2->setTitle("Welcome to USA!");
         $microPost2->setText("Welcome to USA");
+        $microPost2->setAuthor($user1);
         $microPost2->setCreatedAt(new DateTime());
         $manager->persist($microPost2);
 
         $microPost3 = new MicroPost();
         $microPost3->setTitle("Welcome to UK!");
         $microPost3->setText("Welcome to UK");
+        $microPost3->setAuthor($user2);
         $microPost3->setCreatedAt(new DateTime());
         $manager->persist($microPost3);
 
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -22,6 +22,10 @@ class Comment
     #[ORM\JoinColumn(nullable: false)]
     private ?MicroPost $post = null;
 
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -47,6 +51,18 @@ class Comment
     public function setPost(?MicroPost $post): static
     {
         $this->post = $post;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
 
         return $this;
     }
